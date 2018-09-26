2b530c2412808315008514b753d1d35d48e048ce
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-26 17:04:02 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-Assignments/m3/3-sum/Solution.java
+++ ADS-1-Assignments/m3/3-sum/Solution.java
@@ -12,13 +12,9 @@ class Solution {
 		for (int i = 0; i < size - 2; i++) {
 			int j = i + 1;
 			int k = j + 1;
-			while (j < size && k < size) {
-				if (arr[i] + arr[j] + arr[k] == 0) {
-					cnt++;
-				}
-				break;
+			if (arr[i] + arr[j] + arr[k] == 0) {
+				cnt++;
 			}
-			j++;k++;
 		}
 		System.out.println(cnt);
 	}

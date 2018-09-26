7b395ae09bd4d22daa88e286b319dca5ff2c71fa
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-26 15:18:10 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-Assignments/m3/3-sum/Solution.java
+++ ADS-1-Assignments/m3/3-sum/Solution.java
@@ -8,15 +8,14 @@ class Solution {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		int cnt = 0;
-		for (int i = 0; i < size; i++) {
-			for (int j = i + 1; j < size; j++) {
-				for (int k = j + 1; k < size; k++) {
-					if (arr[i] + arr[j] + arr[k] == 0) {
-						cnt++;
-						break;
-					}
-				}
+		int cnt = 0, j = 0, k = 0;
+		for (int i = 0; i < size - 2; i++) {
+			j = i + 1;
+			k = j + 1;
+			if (arr[i] + arr[j] + arr[k] == 0) {
+				cnt++;
+				j++;k++;
+				break;
 			}
 		}
 		System.out.println(cnt);

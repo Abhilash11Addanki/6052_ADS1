5cabf0c0e8e54b1790843a18335799d38bc3e801
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-26 22:10:17 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-Assignments/m3/3-sum/Solution.java
+++ ADS-1-Assignments/m3/3-sum/Solution.java
@@ -1,20 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int[] arr = new int[size];
-		for (int i = 0; i < size; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		int count = 0;
-		for (int i = 0; i < size; i++) {
-			for (int j = i + 1; j < size; j++) {
-				int k = Arrays.binarySearch(arr, -(arr[i] + arr[j]));
-				if (k > j) count++;
-			}
-		}
-		System.out.println(count);
-	}
+    /**
+     * main method that prints the count of pairs.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int k = Arrays.binarySearch(arr, -(arr[i] + arr[j]));
+                if (k > j) count++;
+            }
+        }
+        System.out.println(count);
+    }
 }

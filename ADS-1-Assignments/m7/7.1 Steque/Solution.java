b7eef8dcf96875953b4bd9483102791b9ce2121b
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-01 10:22:20 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS-1-Assignments/m7/7.1 Steque/Solution.java
+++ ADS-1-Assignments/m7/7.1 Steque/Solution.java
@@ -1,34 +1,48 @@
 import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int testcases = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < testcases; i++) {
-			Steque<Integer> st = new Steque<Integer>();
-			while (sc.hasNext()) {
-				String tmp = sc.nextLine();
-				if (tmp.equals("")) {
-					break;
-				}
-				String[] tokens = tmp.split(" ");
-				switch (tokens[0]) {
-				case "push":
-					st.push(Integer.parseInt(tokens[1]));
-					st.display();
-					break;
-				case "pop":
-					st.pop();
-					st.display();
-					break;
-				case "enqueue":
-					st.insertAtEnd(Integer.parseInt(tokens[1]));
-					st.display();
-					break;
-				default:
-					break;
-				}
-			}
-			System.out.println();
-		}
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method to drive the program.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < testcases; i++) {
+            Steque<Integer> st = new Steque<Integer>();
+            while (sc.hasNext()) {
+                String tmp = sc.nextLine();
+                if (tmp.equals("")) {
+                    break;
+                }
+                String[] tokens = tmp.split(" ");
+                switch (tokens[0]) {
+                case "push":
+                    st.push(Integer.parseInt(tokens[1]));
+                    st.display();
+                    break;
+                case "pop":
+                    st.pop();
+                    st.display();
+                    break;
+                case "enqueue":
+                    st.insertAtEnd(
+                        Integer.parseInt(tokens[1]));
+                    st.display();
+                    break;
+                default:
+                    break;
+                }
+            }
+            System.out.println();
+        }
+    }
 }

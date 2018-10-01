b716607821c656a712bfb5108c6f644c5b5b93e0
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-01 14:01:51 +0530

    "check_style_score -> 8 of 12 passed. style score: 0/1 "

--- ADS-1-Assignments/m6-week exam/Assignment-1/Solution.java
+++ ADS-1-Assignments/m6-week exam/Assignment-1/Solution.java
@@ -58,11 +58,11 @@ final class AddLargeNumbers {
         int secele = 0;
         int temp2 = 0;
         while (!s1.isEmpty() && !s2.isEmpty()) {
-            if (s2.isEmpty()) {
+            if (s2.isEmpty() && !s1.isEmpty()) {
                 firstele = Integer.parseInt(s1.pop());
                 temp2 = result / 10;
                 result = firstele + temp2;
-            } else if (s1.isEmpty()) {
+            } else if (s1.isEmpty() && !s2.isEmpty()) {
                 System.out.println("Working");
                 secele = Integer.parseInt(s2.pop());
                 temp2 = result / 10;

f8e3bc2553250c4663055f41d0752d86507f3a0a
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-01 10:41:49 +0530

    "check_style_score -> 6 of 12 passed. style score: 1/1 "

---
+++ ADS-1-Assignments/m6-week exam/Assignment-1/LinkedList.java
@@ -0,0 +1,106 @@
+class LinkedList {
+	class Node {
+		String data;
+		Node link;
+	}
+	Node head;
+	int size;
+	void insertAtStart(String data) {
+		Node node = new Node();
+		size++;
+		if (head == null) {
+			node.data = data;
+			node.link = null;
+			head = node;
+		} else {
+			node.data = data;
+			node.link = head;
+			head = node;
+		}
+	}
+	void insertAtEnd(String data) {
+		Node node = new Node();
+		size++;
+		if (head == null) {
+			node.data = data;
+			node.link = null;
+			head = node;
+		} else {
+			Node temp = head;
+			while (temp.link != null) {
+				temp = temp.link;
+			}
+			node.data = data;
+			temp.link = node;
+			node.link = null;
+		}
+	}
+	void insertAtPos(String data, int pos) {
+		Node node = new Node();
+		if (pos == 0) {
+			return;
+		}
+		if (pos == 1) {
+			insertAtStart(data);
+			return;
+		}
+		pos--;
+		Node temp = head;
+		while (pos != 1) {
+			temp = temp.link;
+			pos--;
+		}
+		node.data = data;
+		node.link = temp.link;
+		temp.link = node;
+		size++;
+	}
+	void deleteAtStart() {
+		Node node = new Node();
+		Node temp = head;
+		temp = temp.link;
+		head.link = null;
+		head = temp;
+		size--;
+	}
+	void deleteAtEnd() {
+		Node node = new Node();
+		Node temp = head;
+		while (temp.link.link != null) {
+			temp = temp.link;
+		}
+		temp.link = null;
+		size--;
+	}
+	void deleteAtPos(int pos) {
+		if (pos == 0) return;
+		if (pos == 1) {
+			deleteAtStart();
+			return;
+		}
+		pos--;
+		Node temp = head;
+		while (pos != 1) {
+			temp = temp.link;
+			pos--;
+		}
+		temp.link = temp.link.link;
+		size--;
+	}
+	String listtoString() {
+		String str = "";
+		if (head.link == null) {
+			str += head.data;
+			return str;
+		}
+		Node first = head;
+		str += head.data;
+		first = head.link;
+		while (first.link != null) {
+			str += first.data;
+			first = first.link;
+		}
+		str += first.data;
+		return str;
+	}
+}
---
+++ ADS-1-Assignments/m6-week exam/Assignment-1/Stack.java
@@ -0,0 +1,43 @@
+class Stack<T> {
+	class Node {
+		T data;
+		Node link;
+	}
+	Node head;
+	int top = -1;
+	void push(T data) {
+		Node node = new Node();
+		++top;
+		if (head == null) {
+			node.data = data;
+			node.link = null;
+			head = node;
+		} else {
+			node.data = data;
+			node.link = head;
+			head = node;
+		}
+	}
+	void pop() {
+		top--;
+		System.out.println(head.data);
+		head = head.link;
+	}
+	void display() {
+		if (top == -1) {
+			System.out.println("Empty");
+			return;
+		}
+		if (head.link == null) {
+			System.out.println("[" + head.data + "]");
+			return;
+		}
+		Node temp = head;
+		System.out.print("[");
+		while (temp.link != null) {
+			System.out.print(temp.data + ", ");
+			temp = temp.link;
+		}
+		System.out.println(temp.data + "]");
+	}
+}

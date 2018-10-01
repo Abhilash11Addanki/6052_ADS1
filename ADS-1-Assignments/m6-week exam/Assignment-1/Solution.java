d6c19fd21d740f4e3b619ad5df5e303a72922c7c
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-01 15:20:17 +0530

    "check_style_score -> 8 of 12 passed. style score: 0/1 "

--- ADS-1-Assignments/m6-week exam/Assignment-1/Solution.java
+++ ADS-1-Assignments/m6-week exam/Assignment-1/Solution.java
@@ -74,8 +74,7 @@ final class AddLargeNumbers {
             }
             res.insertAtStart(Integer.toString(result % 10));
         }
-        res.insertAtStart(Integer.toString(result));
-        res.deleteAtStart();
+        res.insertAtStart(Integer.toString(result / 10));
         return res;
     }
 }

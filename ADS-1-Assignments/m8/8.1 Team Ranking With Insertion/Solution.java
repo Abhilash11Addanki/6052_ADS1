21f62a0f4cdcb418be4983331ae1fe929251bb62
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-02 23:08:14 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1-Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1-Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -18,8 +18,9 @@ public final class Solution {
         Teams t = new Teams();
         while (sc.hasNext()) {
             String[] tokens = sc.nextLine().split(",");
+            final int three = 3;
             t.add(new Team(tokens[0], Integer.parseInt(tokens[1]),
-                           Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3])));
+            Integer.parseInt(tokens[2]), Integer.parseInt(tokens[three])));
         }
         t.sort();
         System.out.println(t);

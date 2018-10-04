dc122e06fa83239bed41aadf38f8c78133d10472
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-04 22:10:51 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,28 +1,42 @@
 import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
-		LinkedList l = new LinkedList();
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			String[] tokens = sc.nextLine().split(" ");
-			switch (tokens[0]) {
-			case "insertAt":
-				try {
-					l.head = l.insertAt(l.head, new Node(Integer.parseInt(tokens[2])),
-					                    Integer.parseInt(tokens[1]), 0);
-					l.size++;
-					l.display();
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case "reverse":
-				l.reverse(l.head, null);
-				l.display();
-				break;
-			default:
-				break;
-			}
-		}
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        LinkedList l = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "insertAt":
+                try {
+                    l.head = l.insertAt(l.head,
+                        new Node(Integer.parseInt(tokens[2])),
+                        Integer.parseInt(tokens[1]), 0);
+                    l.size++;
+                    l.display();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "reverse":
+                l.reverse(l.head, null);
+                l.display();
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }

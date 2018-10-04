56979ffd3276b106180f7d66c0027ed9634ce89b
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-04 19:26:10 +0530

    "ads1_m10_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
 				if (Integer.parseInt(tokens[1]) < 0 || Integer.parseInt(tokens[1]) > l.size) {
 					System.out.println("Can't insert at this position.");
 				} else {
-					l.insert(l.head, Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+					l.insert(l.head, Integer.parseInt(tokens[2]), Integer.parseInt(tokens[1]));
 					l.display();
 				}
 				break;

51cec895f5b05db335ad74e4841063cde83b8fa2
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-08 20:16:27 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-1-Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/QuickSort.java
@@ -0,0 +1,129 @@
+/**
+ * QuickSort class.
+ */
+class QuickSort {
+
+    /**.
+     * Constructs the object.
+     */
+    QuickSort() {
+    }
+// time complexity for this method is O(N^2).
+// In first for loop and second loop it iterates through the size of array
+/**.
+     * insertion method for insertion sort.
+     * @param      arr  The arguments
+     * @param      low  The arguments
+     * @param      high  The arguments
+     */
+    public void insertion(final Comparable[] arr, final int low,
+     final int high) {
+        for (int i = low; i <= high; i++) {
+            for (int j = i; j > low && less(arr[j], arr[j - 1]); j--) {
+                swap(arr, j, j - 1);
+            }
+        }
+    }
+// time complexity of this method is O(1). It swaps the elements only once.
+/**.
+     * swap method for insertion sort.
+     * @param      arr  The arguments
+     * @param      j  The arguments
+     * @param      min  The arguments
+     */
+    public void swap(final Comparable[] arr, final int j, final int min) {
+        Comparable temp = arr[j];
+        arr[j] = arr[min];
+        arr[min] = temp;
+    }
+// time complexity of this method is O(N). It iterates through
+// the array to print all the objects in array.
+
+    /**.
+     * Returns a string representation of the object.
+     * @param      arr   The arr
+     * @return     String representation of the object.
+     */
+    public String toString(final Comparable[] arr) {
+        // System.out.println("in print");
+        String str = "[";
+        for (int i = 0; i < arr.length - 1; i++) {
+            str += arr[i] + ", ";
+        }
+        str += arr[arr.length - 1] + "]";
+        return str;
+    }
+// time complexity is O(log N)
+/**.
+     * sort method for quick sort.
+     * @param      arr  The arguments
+     * @param      low  The arguments
+     * @param      high  The arguments
+     * @param      cutOff  The arguments
+     */
+    public void sort(final Comparable[] arr, final int low,
+     final int high, final int cutOff) {
+        if (high <= low + cutOff - 1) {
+            insertion(arr, low, high);
+            System.out.println("insertionSort called");
+            return;
+        }
+        int k = partition(arr, low, high);
+        System.out.println(toString(arr));
+        sort(arr, low, k - 1, cutOff);
+        sort(arr, k + 1, high, cutOff);
+    }
+//time complexity is O(1)
+
+    /**.
+     * sort method for quick sort.
+     * @param      array   The array
+     * @param      cutOff  The cut off
+     */
+    public void sort(final Comparable[] array, final int cutOff) {
+        sort(array, 0, array.length - 1, cutOff);
+    }
+// time complexity is O(N)
+
+    /**.
+     * partition method for quick sort.
+     * @param      arr   The arr
+     * @param      low   The low
+     * @param      high  The high
+     * @return     index at which arr is partitioned.
+     */
+    public int partition(final Comparable[] arr,
+     final int low, final int high) {
+        int i = low;
+        int j = high + 1;
+        while (true) {
+            while (less(arr[++i], arr[low])) {
+                if (i == high) {
+                    break;
+                }
+            }
+            while (less(arr[low], arr[--j])) {
+                if (j == low) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, low, j);
+        return j;
+    }
+// time complexity is O(1)
+
+    /**.
+     * less method that compares the elements.
+     * @param      a     a of type Comparable
+     * @param      b     b of type Comparable
+     * @return     true or false.
+     */
+    public boolean less(final Comparable a, final Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+}
---
+++ ADS-1-Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/**.
+ * Solution class.
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**.
+     * main method that drives the program.
+     * @param      args  The arguments
+     */
+    // time complexity for the main method is N
+    // Because there is one while loop.
+    // while loop iterates until it has next line i.e N times.
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        QuickSort sort = new QuickSort();
+        int testcases = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < testcases; i++) {
+            int j = Integer.parseInt(sc.nextLine());
+            // sc.nextLine();
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+            if (tokens[0].equals("")) {
+                System.out.println("[]");
+            } else {
+                sort.sort(tokens, j);
+                System.out.println(sort.toString(tokens));
+            }
+        }
+    }
+}

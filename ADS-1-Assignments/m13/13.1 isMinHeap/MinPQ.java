b8ba0e2f31334f260d149f840bc5a600a9b9b829
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-08 20:39:07 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m13/13.1 isMinHeap/MinPQ.java
+++ ADS-1-Assignments/m13/13.1 isMinHeap/MinPQ.java
@@ -1,16 +1,27 @@
+/**
+ * Class for minimum pq.
+ * @param      <Key>  The key
+ */
 public class MinPQ<Key extends Comparable<Key>> {
-	MinPQ() {
-	}
-	public void isMinHeap(final Key[] arr) {
-		int k = arr.length - 1;
-		while(k > 1) {
-			if (arr[k].compareTo(arr[k / 2]) >= 0) {
-				k--;
-			} else {
-				System.out.println("false");
-				return;
-			}
-		}
-		System.out.println("true");
-	}
+    /**
+     * Constructs the object.
+     */
+    MinPQ() {
+    }
+    /**
+     * Determines if minimum heap.
+     * @param      arr   The arr
+     */
+    public void isMinHeap(final Key[] arr) {
+        int k = arr.length - 1;
+        while(k > 1) {
+            if (arr[k].compareTo(arr[k / 2]) >= 0) {
+                k--;
+            } else {
+                System.out.println("false");
+                return;
+            }
+        }
+        System.out.println("true");
+    }
 }
--- ADS-1-Assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS-1-Assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,63 +1,73 @@
 import java.util.Scanner;
+/**
+ * Solution class.
+ */
 public final class Solution {
-	private Solution() {
-	}
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String datatype = sc.nextLine();
-		int testcases = Integer.parseInt(sc.nextLine());
-		switch(datatype) {
-			case "String":
-			while(testcases-- != 0) {
-				String[] arr = sc.nextLine().split(",");
-				String[] strarr = new String[arr.length + 1];
-				for (int j = 0; j < strarr.length - 1; j++) {
-					strarr[j + 1] = arr[j];
-				}
-				MinPQ<String> pq = new MinPQ<String>();
-				pq.isMinHeap(strarr);
-			}
-			break;
-			case "Integer":
-			while(testcases-- != 0) {
-				String[] arr = sc.nextLine().split(",");
-				Integer[] intarr = new Integer[arr.length + 1];
-				for (int j = 0; j < intarr.length - 1; j++) {
-					intarr[j + 1] = Integer.parseInt(arr[j]);
-				}
-				MinPQ<Integer> pq = new MinPQ<Integer>();
-				pq.isMinHeap(intarr);
-			}
-			break;
-			case "Float":
-			while(testcases-- != 0) {
-				String line = sc.nextLine();
-				if (line.equals("")) {
-					System.out.println("false");
-					break;
-				}
-				String[] arr = line.split(",");
-				Float[] floatarr = new Float[arr.length + 1];
-				for (int j = 0; j < floatarr.length - 1; j++) {
-					floatarr[j + 1] = Float.parseFloat(arr[j]);
-				}
-				MinPQ<Float> pq = new MinPQ<Float>();
-				pq.isMinHeap(floatarr);
-			}
-			break;
-			case "Double":
-			while(testcases-- != 0) {
-				String[] arr = sc.nextLine().split(",");
-				Double[] doublearr = new Double[arr.length + 1];
-				for (int j = 0; j < doublearr.length - 1; j++) {
-					doublearr[j + 1] = Double.parseDouble(arr[j]);
-				}
-				MinPQ<Double> pq = new MinPQ<Double>();
-				pq.isMinHeap(doublearr);
-			}
-			break;
-			default:
-			break;
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method to drive the program.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String datatype = sc.nextLine();
+        int testcases = Integer.parseInt(sc.nextLine());
+        switch(datatype) {
+            case "String":
+            while(testcases-- != 0) {
+                String[] arr = sc.nextLine().split(",");
+                String[] strarr = new String[arr.length + 1];
+                for (int j = 0; j < strarr.length - 1; j++) {
+                    strarr[j + 1] = arr[j];
+                }
+                MinPQ<String> pq = new MinPQ<String>();
+                pq.isMinHeap(strarr);
+            }
+            break;
+            case "Integer":
+            while(testcases-- != 0) {
+                String[] arr = sc.nextLine().split(",");
+                Integer[] intarr = new Integer[arr.length + 1];
+                for (int j = 0; j < intarr.length - 1; j++) {
+                    intarr[j + 1] = Integer.parseInt(arr[j]);
+                }
+                MinPQ<Integer> pq = new MinPQ<Integer>();
+                pq.isMinHeap(intarr);
+            }
+            break;
+            case "Float":
+            while(testcases-- != 0) {
+                String line = sc.nextLine();
+                if (line.equals("")) {
+                    System.out.println("false");
+                    break;
+                }
+                String[] arr = line.split(",");
+                Float[] floatarr = new Float[arr.length + 1];
+                for (int j = 0; j < floatarr.length - 1; j++) {
+                    floatarr[j + 1] = Float.parseFloat(arr[j]);
+                }
+                MinPQ<Float> pq = new MinPQ<Float>();
+                pq.isMinHeap(floatarr);
+            }
+            break;
+            case "Double":
+            while(testcases-- != 0) {
+                String[] arr = sc.nextLine().split(",");
+                Double[] doublearr = new Double[arr.length + 1];
+                for (int j = 0; j < doublearr.length - 1; j++) {
+                    doublearr[j + 1] = Double.parseDouble(arr[j]);
+                }
+                MinPQ<Double> pq = new MinPQ<Double>();
+                pq.isMinHeap(doublearr);
+            }
+            break;
+            default:
+            break;
+        }
+    }
 }

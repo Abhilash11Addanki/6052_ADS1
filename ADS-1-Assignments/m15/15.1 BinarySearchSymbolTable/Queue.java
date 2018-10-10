1accd5e3c8502a027a090533edad446c6dc55c5c
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-10 15:24:19 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m15/15.1 BinarySearchSymbolTable/Queue.java
+++ ADS-1-Assignments/m15/15.1 BinarySearchSymbolTable/Queue.java
@@ -2,86 +2,108 @@
  * Queue class.
  * @param      <Item>  The item
  */
-public class Queue<Item> implements Iterable<Item>{
-    /**
-     * first of type Node.
-     */
-    private Node<Item> first;
-    /**
-     * second of type Node.
-     */
-    private Node<Item> last;
-    /**
-     * size.
-     */
-    private int n;
-    /**
-     * Class for node.
-     * @param      <Item>  The item
-     */
-    private static class Node<Item> {
-        /**
-         * item of type Item.
-         */
-        private Item item;
-        /**
-         * next of type Node.
-         */
-        private Node<Item> next;
-    }
-    /**
-     * Initializes an empty queue.
-     */
-    public Queue() {
-        first = null;
-        last  = null;
-        n = 0;
-    }
-    /**
-     * Returns true if this queue is empty.
-     * @return {@code true} if this queue is empty; {@code false} otherwise
-     */
-    public boolean isEmpty() {
-        return first == null;
-    }
-    /**
-     * Returns the number of items in this queue.
-     * @return the number of items in this queue
-     */
-    public int size() {
-        return n;
-    }
-    /**
-     * Adds the item to this queue.
-     * @param  item the item to add
-     */
-    public void enqueue(final Item item) {
-        Node<Item> oldlast = last;
-        last = new Node<Item>();
-        last.item = item;
-        last.next = null;
-        if (isEmpty()) first = last;
-        else           oldlast.next = last;
-        n++;
-    }
-        public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
-    }
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
-        }
-
-        public boolean hasNext()  { return current != null;                     }
-
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
-    }
+public class Queue<Item> implements Iterable<Item> {
+	/**
+	 * first of type Node.
+	 */
+	private Node<Item> first;
+	/**
+	 * second of type Node.
+	 */
+	private Node<Item> last;
+	/**
+	 * size.
+	 */
+	private int n;
+	/**
+	 * Class for node.
+	 * @param      <Item>  The item
+	 */
+	private static class Node<Item> {
+		/**
+		 * item of type Item.
+		 */
+		private Item item;
+		/**
+		 * next of type Node.
+		 */
+		private Node<Item> next;
+	}
+	/**
+	 * Initializes an empty queue.
+	 */
+	public Queue() {
+		first = null;
+		last  = null;
+		n = 0;
+	}
+	/**
+	 * Returns true if this queue is empty.
+	 * @return {@code true} if this queue is empty; {@code false} otherwise
+	 */
+	public boolean isEmpty() {
+		return first == null;
+	}
+	/**
+	 * Returns the number of items in this queue.
+	 * @return the number of items in this queue
+	 */
+	public int size() {
+		return n;
+	}
+	/**
+	 * Adds the item to this queue.
+	 * @param  item the item to add
+	 */
+	public void enqueue(final Item item) {
+		Node<Item> oldlast = last;
+		last = new Node<Item>();
+		last.item = item;
+		last.next = null;
+		if (isEmpty()) first = last;
+		else           oldlast.next = last;
+		n++;
+	}
+	/**
+	 * iterator method.
+	 * @return     Iterator.
+	 */
+	public Iterator<Item> iterator()  {
+		return new ListIterator<Item>(first);
+	}
+	/**
+	 * Class for list iterator.
+	 * @param      <Item>  The item
+	 */
+	private class ListIterator<Item> implements Iterator<Item> {
+		/**
+		 * current of type Node.
+		 */
+		private Node<Item> current;
+		/**
+		 * Constructs the object.
+		 * @param      first  The first
+		 */
+		public ListIterator(final Node<Item> first) {
+			current = first;
+		}
+		/**
+		 * Determines if it has next.
+		 * @return     True if has next, False otherwise.
+		 */
+		public boolean hasNext() {
+			return current != null;
+		}
+		/**
+		 * next method that returns the elements.
+		 *
+		 * @return     item.
+		 */
+		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+	}
 }

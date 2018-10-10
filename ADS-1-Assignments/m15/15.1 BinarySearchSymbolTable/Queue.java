fe30874f11f85fd3c2ba61bc18e8ce24ab869949
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-10 15:08:22 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m15/15.1 BinarySearchSymbolTable/Queue.java
+++ ADS-1-Assignments/m15/15.1 BinarySearchSymbolTable/Queue.java
@@ -1,15 +1,34 @@
-import java.util.Iterator;
-public class Queue<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of queue
-    private Node<Item> last;     // end of queue
-    private int n;               // number of elements on queue
-
-    // helper linked list class
+/**
+ * Queue class.
+ * @param      <Item>  The item
+ */
+public class Queue<Item> {
+    /**
+     * first of type Node.
+     */
+    private Node<Item> first;
+    /**
+     * second of type Node.
+     */
+    private Node<Item> last;
+    /**
+     * size.
+     */
+    private int n;
+    /**
+     * Class for node.
+     * @param      <Item>  The item
+     */
     private static class Node<Item> {
+        /**
+         * item of type Item.
+         */
         private Item item;
+        /**
+         * next of type Node.
+         */
         private Node<Item> next;
     }
-
     /**
      * Initializes an empty queue.
      */
@@ -18,44 +37,25 @@ public class Queue<Item> implements Iterable<Item> {
         last  = null;
         n = 0;
     }
-
     /**
      * Returns true if this queue is empty.
-     *
      * @return {@code true} if this queue is empty; {@code false} otherwise
      */
     public boolean isEmpty() {
         return first == null;
     }
-
     /**
      * Returns the number of items in this queue.
-     *
      * @return the number of items in this queue
      */
     public int size() {
         return n;
     }
-
-    /**
-     * Returns the item least recently added to this queue.
-     *
-     * @return the item least recently added to this queue
-     * @throws NoSuchElementException if this queue is empty
-     */
-    public Item peek() {
-        if (isEmpty()) {
-            return null;
-        }
-        return first.item;
-    }
-
     /**
      * Adds the item to this queue.
-     *
      * @param  item the item to add
      */
-    public void enqueue(Item item) {
+    public void enqueue(final Item item) {
         Node<Item> oldlast = last;
         last = new Node<Item>();
         last.item = item;
@@ -63,33 +63,5 @@ public class Queue<Item> implements Iterable<Item> {
         if (isEmpty()) first = last;
         else           oldlast.next = last;
         n++;
-    }
-    public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);
-    }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
-        }
-
-        public boolean hasNext() {
-            return current != null;
-        }
-        public void remove() {
-            return;
-        }
-
-        public Item next() {
-            if (!hasNext()) {
-                return null;
-            }
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
     }
 }

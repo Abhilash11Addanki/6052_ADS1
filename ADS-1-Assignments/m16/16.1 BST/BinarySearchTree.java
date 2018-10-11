37ef06e6169bc022a6dc2b263bc019cc9da5919e
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-11 20:22:54 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m16/16.1 BST/BinarySearchTree.java
+++ ADS-1-Assignments/m16/16.1 BST/BinarySearchTree.java
@@ -1,49 +1,113 @@
-class BinarySearchTree<Key extends Comparable<Key>, Value> {
-	private Node root;
-	public class Node {
-		private Key key;
-		private Value value;
-		private Node left, right;
-		Node(final Key k, final Value v) {
-			key = k;
-			value = v;
-		}
-	}
-	Value get(Key k) {
-		Node n = root;
-		if (k == null) {
-			return null;
-		}
-		if (n == null) {
-			return null;
-		}
-		int cmp = k.compareTo(n.key);
-		if (cmp < 0) {
-			n = n.left;
-		}
-		if (cmp > 0) {
-			n = n.right;
-		}
-		if (cmp == 0) {
-			return n.value;
-		}
-		return null;
-	}
-	void put(Key k, Value val) {
-		root = put(root, k, val);
-	}
-	private Node put(Node n, Key k, Value value) {
-		if (n == null) {
-			return new Node(k, value);
-		}
-		int cmp = k.compareTo(n.key);
-		if (cmp < 0) {
-			n.left = put(n.left, k, value);
-		} else if (cmp > 0) {
-			n.right = put(n.right, k, value);
-		} else {
-			n.value = value;
-		}
-		return n;
-	}
+class BinarySearchTree<Key extends Comparable<Key>, Values> {
+    /**.
+     * starting element in the BST.
+     */
+    private Node root;
+    /**.
+     * Class for node.
+     */
+    class Node {
+        /**.
+         * Object of Book class with book details.
+         */
+        private Key key;
+        /**.
+         * value of the book in Book class
+         */
+        private Values value;
+        /**.
+         * Right node of the BST
+         */
+        private Node right;
+        /**.
+         * Left node of BST.
+         */
+        private Node left;
+        /**.
+         * Constructs the object.
+         *
+         * @param      key1  The key 1
+         * @param      val   The value
+         */
+        Node(final Key key1, final Values val) {
+            this.key = key1;
+            this.value = val;
+        }
+
+    }
+    /**.
+     * This method is to add a key and value to BST.
+     * Time Complexity is O(1).
+     * because it calls another put method to add elements.
+     *
+     * @param      key    The key
+     * @param      value  The value
+     */
+    public void put(final Key key, final Values value) {
+        if (key == null) {
+            return;
+        }
+        root = put(root, key, value);
+    }
+    /**.
+     * This method is to add element to BST
+     * Time Complexity is O(N) for worst case.
+     * element is added until it reaches to the
+     * position it should added at.
+     *
+     * @param      node   The node
+     * @param      key    The key
+     * @param      value  The value
+     *
+     * @return     returns node
+     */
+    public Node put(final Node node, final Key key,
+                    final Values value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+    /**.
+     * This method is to return the value of that key
+     * Time Complexity is O(N)
+     *
+     * @param      key   The key
+     *
+     * @return     returns the value.
+     */
+    public Values get(final Key key) {
+        return get(root, key);
+    }
+    /**.
+     * This method is to return the value of that key
+     * Time Complexity is O(N)
+     *
+     * @param      node  the node where the book details and
+     *                   values.
+     * @param      key   The key
+     *
+     * @return     returns the value of that key.
+     */
+    private Values get(final Node node, final Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node.value;
+        }
+    }
 }

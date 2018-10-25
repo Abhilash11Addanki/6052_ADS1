067b26c4bdb5c9c212495cad59d954c3dd8b0d7e
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-25 17:39:17 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m22/22.1 Ransom Note/SequentialSearchST.java
+++ ADS-1-Assignments/m22/22.1 Ransom Note/SequentialSearchST.java
@@ -22,44 +22,29 @@ public class SequentialSearchST<Key, Value> {
         return n;
     }
     /**
-     * Returns true if this symbol table contains the specified key.
-     *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key};
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * Checks whether the key is present in the symbol table or not.
+     * @param      key   The key
+     * @return     true or false.
      */
-    public boolean contains(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+    public boolean contains(final Key key) {
         return get(key) != null;
     }
-
     /**
-     * Returns the value associated with the given key in this symbol table.
-     *
-     * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * gets the value of the particular key in the symbol table.
+     * @param      key   The key
+     * @return     Value of type Integer.
      */
-    public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+    public Value get(final Key key) {
         for (Node x = first; x != null; x = x.next) {
             if (key.equals(x.key))
                 return x.val;
         }
         return null;
     }
-
     /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * inserts key value pair into the symbol table.
+     * @param      key   The key
+     * @param      val   The value
      */
     public void put(final Key key, final Value val) {
         for (Node x = first; x != null; x = x.next) {
@@ -70,18 +55,5 @@ public class SequentialSearchST<Key, Value> {
         }
         first = new Node(key, val, first);
         n++;
-    }
-    /**
-     * Returns all keys in the symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     *
-     * @return all keys in the symbol table
-     */
-    public Iterable<Key> keys()  {
-        Queue<Key> queue = new Queue<Key>();
-        for (Node x = first; x != null; x = x.next)
-            queue.enqueue(x.key);
-        return queue;
     }
 }

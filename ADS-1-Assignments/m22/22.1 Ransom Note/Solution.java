1bfcd196c231e767c535bd64e1f15110a9370281
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-25 17:54:14 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m22/22.1 Ransom Note/Solution.java
+++ ADS-1-Assignments/m22/22.1 Ransom Note/Solution.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
-class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+	}
+	/**
+	 * main method that drives the program.
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] inp = sc.nextLine().split(" ");
 		int m = Integer.parseInt(inp[0]);
 		int n = Integer.parseInt(inp[1]);
 		String[] magazine = sc.nextLine().split(" ");
 		String[] note = sc.nextLine().split(" ");
-		SeparateChainingHashST<String, Integer> mag = new SeparateChainingHashST<String, Integer>();
-		SeparateChainingHashST<String, Integer> notes = new SeparateChainingHashST<String, Integer>();
+		SeparateChainingHashST<String, Integer> mag = 
+		new SeparateChainingHashST<String, Integer>();
+		SeparateChainingHashST<String, Integer> notes = 
+		new SeparateChainingHashST<String, Integer>();
 		for (int i = 0; i < m; i++) {
 			mag.put(magazine[i], 0);
 		}

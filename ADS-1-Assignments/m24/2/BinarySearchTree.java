855fee7cc9c1b8d523aa6ef84ebc1c751667e09a
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-27 13:12:43 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-Assignments/m24/2/BinarySearchTree.java
+++ ADS-1-Assignments/m24/2/BinarySearchTree.java
@@ -76,6 +76,8 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
             node.left = put(node.left, key, value);
         } else if (cmp > 0) {
             node.right = put(node.right, key, value);
+        } else if (cmp == 0) {
+            node.left = put(node.left, key, value);
         } else {
             node.value = value;
         }

f2b522413845d1145244196a31a7c222de6e88c3
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-27 10:39:10 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-Assignments/m24/1/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SeparateChainingHashST<Integer, String> h1 = new SeparateChainingHashST();
+        SeparateChainingHashST<Integer, Double> h2 = new SeparateChainingHashST();
+        int n = Integer.parseInt(sc.nextLine());
+        while (n-- != 0) {
+            String[] inp = sc.nextLine().split(",");
+            h1.put(Integer.parseInt(inp[0]), inp[1]);
+            h2.put(Integer.parseInt(inp[0]), Double.parseDouble(inp[2]));
+        }
+        int testcases = Integer.parseInt(sc.nextLine());
+        while (testcases-- != 0) {
+            String[] tokens = sc.nextLine().split(" ");
+            if (tokens[0].equals("get") && tokens[2].equals("1")) {
+                System.out.println(h1.get(Integer.parseInt(tokens[1])));
+            } else if (tokens[0].equals("get") && tokens[2].equals("2")) {
+                System.out.println(h2.get(Integer.parseInt(tokens[1])));
+            }
+        }
+    }
+}

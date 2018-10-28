463b33c1943bb962ffb90875690d17eeb217e837
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-28 17:00:28 +0530

    "m24_a2_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m24/2/Student.java
+++ ADS-1-Assignments/m24/2/Student.java
@@ -1,31 +1,73 @@
+/**
+ * Class for student.
+ */
 class Student implements Comparable<Student> {
-	private String roll_number;
-	private String student_name;
-	private double total_marks;
-	Student(String roll, String name, double marks) {
-		roll_number = roll;
-		student_name = name;
-		total_marks = marks;
-	}
-	String getRollNumber() {
-		return roll_number;
-	}
-	String getStudentName() {
-		return student_name;
-	}
-    double getTotalMarks() {
-    	return total_marks;
+    /**
+     * roll number of Student.
+     */
+    private String roll_number;
+    /**
+     * name of the Student.
+     */
+    private String student_name;
+    /**
+     * total marks of the Student.
+     */
+    private double total_marks;
+    /**
+     * Constructs the object.
+     * @param      roll   The roll
+     * @param      name   The name
+     * @param      marks  The marks
+     */
+    Student(final String roll, final String name,
+        final double marks) {
+        roll_number = roll;
+        student_name = name;
+        total_marks = marks;
     }
-	public int compareTo(final Student s) {
-		if (total_marks > s.total_marks) {
-			return 1;
-		}
-		if (total_marks < s.total_marks) {
-			return -1;
-		}
-		return roll_number.compareTo(s.roll_number);
-	}
-	public String toString() {
-		return roll_number + student_name + Double.toString(total_marks);
-	}
+    /**
+     * Gets the roll number.
+     * @return     The roll number.
+     */
+    String getRollNumber() {
+        return roll_number;
+    }
+    /**
+     * Gets the student name.
+     * @return     The student name.
+     */
+    String getStudentName() {
+        return student_name;
+    }
+    /**
+     * Gets the total marks.
+     * @return     The total marks.
+     */
+    double getTotalMarks() {
+        return total_marks;
+    }
+    /**
+     * compareTo method that compares the marks
+     * and then roll number.
+     * @param      s     s of type Student.
+     * @return     integer.
+     */
+    public int compareTo(final Student s) {
+        if (total_marks > s.total_marks) {
+            return 1;
+        }
+        if (total_marks < s.total_marks) {
+            return -1;
+        }
+        return roll_number.compareTo(s.roll_number);
+    }
+    /**
+     * Returns a string representation of the object.
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return roll_number + student_name
+        + Double.toString(total_marks);
+    }
 }
